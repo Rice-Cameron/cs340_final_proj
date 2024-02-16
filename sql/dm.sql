7eb8376e923e7e8ef415d9f82fe44cdcdf613436
Rice-Cameron <cameron5237@gmail.com>
2024-02-15 17:26:08 -0800

    Update sql

--- sql/dm.sql
+++ sql/dm.sql
@@ -4,8 +4,8 @@
 
 -- Query to add a review to a book
 INSERT INTO Reviews (userID, isbn, rating, reviewText)
-VALUES (:userID, :isbn, :rating, :reviewText);
+VALUES (:userID_from_account, :isbn_from_book_title, :ratingInput, :reviewTextInput);
 
 -- Query to check out a book to a userID
 INSERT INTO Books_Users (isbn, userID, dateBorrowed, dueDate)
-VALUES (:isbn, :userID, :dateBorrowed, :dueDate);
+VALUES (:isbn_from_book_title, :userID, :dateBorrowed_from_current_date, :dueDate_by_adding_checkout_period);
