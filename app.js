b46868d80fd24f2f904cb2f0c1085a55b0f0d7af
Cameron Rice <ricecam@oregonstate.edu>
2024-02-27 17:34:16 -0800

    Create placeholder hbs files

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // App.js
+
 /*
     SETUP
 */
+
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 6731;                 // Set a port number at the top so it's easy to change in the future
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+// database
+var db = require('./database/db-connector')
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-{
-    res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
-});                                         // requesting the web site.
+
+app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
