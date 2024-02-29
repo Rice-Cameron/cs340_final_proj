46fbb216b118f0a27ce53751581c1fd75d40bf0d
Cameron Rice <ricecam@oregonstate.edu>
2024-02-29 12:41:36 -0800

    Remove excess comments

--- app.js
+++ app.js
@@ -4,17 +4,17 @@
     SETUP
 */
 
-var express = require('express');   // We are using the express library for the web server
-var app = express();            // We need to instantiate an express object to interact with the server in our code
+var express = require('express');  
+var app = express();            
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
-PORT = 6731;                 // Set a port number at the top so it's easy to change in the future
+PORT = 6731;              
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
-
+var exphbs = require('express-handlebars');    
+app.engine('.hbs', engine({extname: ".hbs"})); 
+app.set('view engine', '.hbs');                
+ 
 // database
 var db = require('./database/db-connector');
 const e = require('express');
@@ -24,33 +24,25 @@ const e = require('express');
 */
 app.get('/', function(req, res)
 {
-    // Declare Query 1
     let query1;
 
-    // If there is no query string, we just perform a basic SELECT
     if (req.query.title === undefined)
     {
         query1 = "SELECT * FROM Books;";
     }
 
-    // If there is a query string, we assume this is a search, and return desired results
     else
     {
         query1 = `SELECT * FROM Books WHERE title LIKE "${req.query.title}%"`
     }
 
-    // Query 2 is the same in both cases
     let query2 = "SELECT * FROM Publishers;";
 
-    // Run the 1st query
     db.pool.query(query1, function(error, rows, fields){
         
-        // Save the books
         let books = rows;
         
-        // Run the second query
         db.pool.query(query2, (error, rows, fields) => {
-            // Save the publishers
             let publishers = rows;
 
             let pubmap = {}
@@ -87,7 +79,6 @@ app.post('/add-book-form', function(req, res){
 })
 
 app.put('/put-book-ajax', function(req,res,next){
-    // debugger;
     let data = req.body;
     
     let isbn = data.isbn
@@ -144,20 +135,15 @@ app.delete('/delete-book-ajax/', function(req,res,next){
     let deleteBooksGenres = `DELETE FROM Books_Genres WHERE isbn = ?`
     let deleteBooksUsers = `DELETE FROM Books_Users WHERE isbn = ?`
     let deleteBook = `DELETE FROM Books WHERE isbn = ?`;
-    
-  
-  
-          // Run the 1st query
-          db.pool.query(deleteBooksAuthors, [isbn], function(error, rows, fields){
+
+    db.pool.query(deleteBooksAuthors, [isbn], function(error, rows, fields){
               if (error) {
   
-              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
               console.log(error);
               res.sendStatus(400);
               }
               else
               {
-                  // Run the second query
                   db.pool.query(deleteBooksGenres, [isbn], function(error, rows, fields) {
   
                       if (error) {
@@ -190,6 +176,6 @@ app.delete('/delete-book-ajax/', function(req,res,next){
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
