929b2491e4eaeeb82893313afcf7c41adf2efb31
Brandon Healey <healeyb@oregonstate.edu>
2024-03-07 13:25:47 -0800

    added fuctionality to genres

--- routes/genres.js
+++ routes/genres.js
@@ -4,12 +4,47 @@ const router = express.Router()
 var db = require('../database/db-connector')
 
 router.use((req, res, next) => {
-    next()
-  })
-
-
-router.get('/genres', (req, res) => {
-   res.render('genres') 
+  next()
 })
 
-module.exports = router
+
+router.get("/genres", function (req, res) {
+  let query1 = "SELECT * FROM Genres;";
+
+  db.pool.query(query1, function (error, rows, fields) {
+
+    return res.render("genres", { data: rows });
+  });
+});
+
+router.post("/add-genre-form", function (req, res) {
+  let data = req.body;
+  let query1 = `INSERT INTO Genres (genreName) 
+                  VALUES 
+                  ('${data["input-name"]}')`;
+  db.pool.query(query1, function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    } else {
+      res.redirect("/genres");
+    }
+  });
+});
+
+router.delete('/delete-genre-ajax/', function (req, res) {
+  let data = req.body;
+  let query = `DELETE FROM Genres WHERE genreID = ?`;
+  db.pool.query(query, [data.genreID], function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
+    else {
+      res.sendStatus(204);
+    }
+  });
+})
+
+module.exports = router;
+
