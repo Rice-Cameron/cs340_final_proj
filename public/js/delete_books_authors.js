3b7981475336bfd52052c9922bf74eebae715c4a
Rice-Cameron <cameron5237@gmail.com>
2024-03-06 21:01:45 -0800

    Fix delete req BA

--- public/js/delete_books_authors.js
+++ public/js/delete_books_authors.js
@@ -16,7 +16,7 @@ function deleteAuthor(authorshipID) {
   }
   
   function deleteRow(authorshipID) {
-    let table = document.getElementById("author-table");
+    let table = document.getElementById("books-authors-table");
     for (let i = 0, row; (row = table.rows[i]); i++) {
       if (table.rows[i].getAttribute("data-value") == authorshipID) {
         table.deleteRow(i);
